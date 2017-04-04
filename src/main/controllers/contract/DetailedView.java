ee1d72531428c1c57bb477e41a49c638b4b42579
Zino Kader <zinokad@gmail.com>
2017-04-04 04:08:25 +0200

    Commented

--- src/main/controllers/contract/DetailedView.java
+++ src/main/controllers/contract/DetailedView.java
@@ -1,7 +1,11 @@
 package main.controllers.contract;
 
+/**
+ * Interface for basic implementation of a detailed (movie/person/series) controller
+ */
 public interface DetailedView {
 
     void delegateSetData();
     void setBaseDetails();
+
 }
