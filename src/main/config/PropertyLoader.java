10f2acc81ff502d6d201ffca260c343136ae519b
zinokader <zino@zinokader.se>
2017-04-09 23:27:12 +0200

    Fixed property file not working (I wasn't saving them to a file).

--- src/main/config/PropertyLoader.java
+++ src/main/config/PropertyLoader.java
@@ -1,42 +1,68 @@
 package main.config;
 
+import com.esotericsoftware.minlog.Log;
+import main.CineMateApplication;
+import main.constants.TmdbConstants;
+import main.exceptions.EmptyValueException;
 import main.exceptions.PropertyAccessException;
 import main.exceptions.PropertyLoadException;
 
-import java.io.IOException;
+import java.io.*;
 import java.util.Properties;
 
 /**
  * Methods in this class are made protected to only allow usage from its subclasses.
  */
 
-public abstract class PropertyLoader {
+abstract class PropertyLoader {
 
-    public static final String PROPERTIES_PATH = "config/settings.properties";
+    private static final String PROPERTIES_PATH = "settings.properties";
     private Properties properties = new Properties();
 
     protected PropertyLoader() {
     }
 
     protected void initializeProperties() throws PropertyLoadException {
+
+        handlePropertiesFileCreation();
+
         try {
-            properties.load((getClass().getClassLoader().getResourceAsStream(PROPERTIES_PATH)));
+            properties.load(new FileInputStream(new File(PROPERTIES_PATH)));
         } catch (IOException exception) {
+            exception.printStackTrace();
             throw new PropertyLoadException(exception);
         }
     }
 
-
-    protected void store(SettingsKey key, String value) {
-        properties.put(key, value);
+    private void handlePropertiesFileCreation() {
+        if(getClass().getClassLoader().getResourceAsStream(PROPERTIES_PATH) == null) {
+            try {
+                //Create property file if non-existent
+                if(new File(PROPERTIES_PATH).createNewFile()) {
+                    Log.debug("Created new properties file.");
+                    put(SettingsKey.API_KEY, ""); //add empty API KEY property
+                } else {
+                    Log.debug("Did not create new properties file. File already exists.");
+                }
+            } catch (IOException e) {
+                Log.debug("Could not create new properties file. " + e.getCause());
+                e.printStackTrace();
+            }
+        }
     }
 
-    protected String get(SettingsKey key) throws PropertyAccessException {
-        String value = String.valueOf(properties.get(key));
+
+    protected void put(SettingsKey key, String value) throws IOException {
+        properties.setProperty(String.valueOf(key), value);
+        properties.store(new FileOutputStream(PROPERTIES_PATH), "");
+    }
+
+    protected String get(SettingsKey key) throws EmptyValueException {
+        String value = properties.getProperty(String.valueOf(key));
         if(value != null) {
-            return String.valueOf(properties.get(key));
+            return value;
         } else {
-            throw new PropertyAccessException();
+            throw new EmptyValueException(key);
         }
     }
 
