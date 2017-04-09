8344d791af41113c0a995b4fd6c00e6f8b85b824
zinokader <zino@zinokader.se>
2017-04-09 23:32:35 +0200

    Handle delayed tasks only after window is shown and after a set delay

--- src/main/controllers/implementation/MainScreenController.java
+++ src/main/controllers/implementation/MainScreenController.java
@@ -1,13 +1,16 @@
 package main.controllers.implementation;
 
+import com.esotericsoftware.minlog.Log;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXListView;
 import com.jfoenix.controls.JFXTextField;
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.Parent;
 import javafx.scene.control.ListView;
 import javafx.scene.layout.StackPane;
 import main.CineMateApplication;
@@ -17,10 +20,10 @@ import main.config.UserSettings;
 import main.constants.FXConstants;
 import main.controllers.ScreenController;
 import main.controllers.contract.ControlledScreen;
+import main.exceptions.EmptyValueException;
 import main.exceptions.PropertyAccessException;
 import main.exceptions.PropertyLoadException;
 import main.helpers.MessageHelper;
-import main.helpers.QueryHelper;
 import main.model.MediaType;
 import main.model.Movie;
 import main.model.Person;
@@ -31,21 +34,24 @@ import main.view.MovieListViewCell;
 import main.view.PersonListViewCell;
 import main.view.SeriesListViewCell;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.List;
-import java.util.Map;
 import java.util.ResourceBundle;
 
 public class MainScreenController implements Initializable, ControlledScreen {
+
+    @FXML
+    private Parent rootParent;
+
+    @FXML
+    private StackPane rootPane;
 
 	@FXML
 	private JFXTextField searchTextField;
 
 	@FXML
 	private JFXComboBox<SearchType> searchTypeBox;
-
-	@FXML
-	private StackPane searchPane;
 
 	@FXML
 	private JFXListView<Movie> movieListView;
@@ -65,14 +71,14 @@ public class MainScreenController implements Initializable, ControlledScreen {
 	private ObservableList<Person> personObservableList = FXCollections.observableArrayList();
 
 	private SearchType currentSearchType;
-	private ScreenController screenController;
+	private ScreenController screenParent;
 	private UserSettings userSettings;
 	private MessageHelper messageHelper;
 	private ApiAdapater apiAdapter;
 	private ApiService apiService;
 
-	/**
-	 * Types you can search for in the searchfield
+    /**
+	 * Types you can search for in the search field
 	 */
 	public enum SearchType {
 		MOVIES, SERIES, PEOPLE
@@ -88,19 +94,33 @@ public class MainScreenController implements Initializable, ControlledScreen {
 		listenToItemClicks(movieListView);
 		listenToItemClicks(seriesListView);
 		listenToItemClicks(personListView);
+		listenToWindowShown();
 
-		messageHelper = new MessageHelper(searchPane);
-
-		try {
-			userSettings = new UserSettings();
-		} catch (PropertyLoadException e) {
-			e.printStackTrace();
-		}
-		apiAdapter = new ApiAdapater();
-		apiService = apiAdapter.getApiService();
-
+		messageHelper = new MessageHelper(rootPane);
 
 	}
+
+	@Override
+	public void listenToWindowShown() {
+	    //Because of JavaFX tomfoolery, we have to observe the parent node to know when the window is shown
+        Platform.runLater( () -> rootParent.sceneProperty().addListener((sceneObservable, oldScene, newScene) -> loadUserSettings()));
+    }
+
+    private void loadUserSettings() {
+        try {
+            userSettings = new UserSettings();
+        } catch (PropertyLoadException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            apiAdapter = new ApiAdapater(userSettings.getApiKey());
+        } catch (EmptyValueException e) {
+            e.printStackTrace();
+        }
+
+        apiService = apiAdapter.getApiService();
+    }
 
 	private void listenToSearchTypeChanges() {
 		searchTypeBox.getSelectionModel().selectedItemProperty().addListener( (observable, oldSearchType, newSearchType) ->  {
@@ -110,21 +130,20 @@ public class MainScreenController implements Initializable, ControlledScreen {
 	}
 
 	private void listenToItemClicks(ListView<?> listView) {
-
-		listView.setOnMouseClicked( clickEvent -> {
-			if(!listView.getSelectionModel().isEmpty() && clickEvent.getClickCount() == FXConstants.DOUBLE_CLICK_COUNT) {
+	    listView.setOnMouseClicked( clickEvent -> {
+	        if(!listView.getSelectionModel().isEmpty() && clickEvent.getClickCount() == FXConstants.DOUBLE_CLICK_COUNT) {
 				switch(currentSearchType) {
 					case MOVIES:
 						Movie selectedMovie = movieListView.getSelectionModel().getSelectedItem();
-						screenController.loadWindow(CineMateApplication.MOVIE_WINDOW_FXML, selectedMovie);
+						screenParent.loadWindow(CineMateApplication.MOVIE_WINDOW_FXML, selectedMovie);
 						break;
 					case SERIES:
 						Series selectedSeries = seriesListView.getSelectionModel().getSelectedItem();
-						screenController.loadWindow(CineMateApplication.SERIES_WINDOW_FXML, selectedSeries);
+						screenParent.loadWindow(CineMateApplication.SERIES_WINDOW_FXML, selectedSeries);
 						break;
 					case PEOPLE:
 						Person selectedPerson = personListView.getSelectionModel().getSelectedItem();
-						screenController.loadWindow(CineMateApplication.PERSON_WINDOW_FXML, selectedPerson);
+						screenParent.loadWindow(CineMateApplication.PERSON_WINDOW_FXML, selectedPerson);
 						break;
 				}
 			}
@@ -134,13 +153,18 @@ public class MainScreenController implements Initializable, ControlledScreen {
 
 	public void searchButtonPressed() throws PropertyAccessException {
 		if(!searchTextField.getText().isEmpty() && currentSearchType != null) {
-			handleSearch(QueryHelper.createQueryMap(userSettings.getApiKey(), searchTextField.getText()));
-		} else {
+            try {
+                handleSearch(searchTextField.getText());
+            } catch (IOException e) {
+                messageHelper.showMessage("Could not fetch search results from TMDB");
+                e.printStackTrace();
+            }
+        } else {
 			messageHelper.showMessage("You need to choose a search type and enter a query to search");
 		}
 	}
 
-	private void handleSearch(Map<String, String> apiQueries) {
+	private void handleSearch(String query) throws IOException {
 		//All of our search objects implement TmdbObject, "? extends ..." really means "is a subclass of ..."
 		ResultsPager<? extends TmdbObject> searchResults = new ResultsPager<>();
 		MediaType mediaType = MediaType.NONE;
@@ -148,15 +172,15 @@ public class MainScreenController implements Initializable, ControlledScreen {
 		switch(currentSearchType) {
 			case MOVIES:
 				mediaType = MediaType.MOVIE;
-				searchResults = apiService.searchMovies(apiQueries);
+				searchResults = apiService.searchMovies(query).execute().body();
 				break;
 			case SERIES:
 				mediaType = MediaType.SERIES;
-				searchResults = apiService.searchSeries(apiQueries);
+				searchResults = apiService.searchSeries(query).execute().body();
 				break;
 			case PEOPLE:
 				mediaType = MediaType.PERSON;
-				searchResults = apiService.searchPeople(apiQueries);
+				searchResults = apiService.searchPeople(query).execute().body();
 				break;
 		}
 
@@ -171,7 +195,7 @@ public class MainScreenController implements Initializable, ControlledScreen {
 	@SuppressWarnings("unchecked")
 	private void populateList(List<? extends TmdbObject> listObjects, MediaType mediaType) {
 		int searchPaneIndex = 0;
-		List<Node> searchResultListView = searchPane.getChildren();
+		List<Node> searchResultListView = rootPane.getChildren();
 
 		//find the index in the stackpane of our desired listview to show (they get shuffled around as they're reordered)
 		switch(mediaType) {
@@ -213,14 +237,13 @@ public class MainScreenController implements Initializable, ControlledScreen {
 				break;
 		}
 
-		searchPane.getChildren().get(searchPaneIndex).toFront();
+		rootPane.getChildren().get(searchPaneIndex).toFront();
 
 	}
 
-
-	@Override
-	public void setScreenParent(final ScreenController screenController) {
-		this.screenController = screenController;
+    @Override
+	public void setScreenParent(ScreenController screenParent) {
+		this.screenParent = screenParent;
 	}
 
 
--- src/main/controllers/implementation/StartupScreenController.java
+++ src/main/controllers/implementation/StartupScreenController.java
@@ -1,100 +1,151 @@
 package main.controllers.implementation;
 
+import com.esotericsoftware.minlog.Log;
 import com.jfoenix.controls.JFXTextField;
 import javafx.application.Platform;
-import javafx.event.ActionEvent;
+import javafx.concurrent.Task;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.layout.VBox;
+import javafx.scene.Parent;
 import main.CineMateApplication;
 import main.api.ApiAdapater;
 import main.api.ApiService;
 import main.config.UserSettings;
+import main.constants.FXConstants;
 import main.controllers.ScreenController;
 import main.controllers.contract.ControlledScreen;
-import main.exceptions.PropertyAccessException;
+import main.exceptions.EmptyValueException;
 import main.exceptions.PropertyLoadException;
+import main.helpers.DelayedTaskHelper;
 import main.helpers.MessageHelper;
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 /**
  * Controller for startup screen. Handles implementations of logic.
  */
-public class StartupScreenController implements Initializable, ControlledScreen
-{
+public class StartupScreenController implements Initializable, ControlledScreen {
 
-	private ScreenController screenController;
-	private UserSettings settings;
+	private ScreenController screenParent;
+	private UserSettings userSettings;
 	private MessageHelper messageHelper;
-	private ApiAdapater apiAdapater = new ApiAdapater();
-	private ApiService apiService = apiAdapater.getApiService();
+	private DelayedTaskHelper delayedTaskHelper;
 
-	//@FXML exposes the fields to the fxml file while keeping the fields private, nice!
+	private ApiAdapater apiAdapater;
+	private ApiService apiService;
 
 	@FXML
-	private VBox startupPane;
+	private Parent rootParent;
 
 	@FXML
 	private JFXTextField apiKeyTextField;
 
-	@Override public void initialize(final URL location, final ResourceBundle resources) {
+    private String apiKey;
 
-		messageHelper = new MessageHelper(startupPane);
+    @Override
+    public void initialize(final URL location, final ResourceBundle resources) {
 
-		try {
-			settings = new UserSettings();
-		} catch (PropertyLoadException e) {
-			e.printStackTrace();
-		}
-
-		assert settings != null;
-
-		String apiKey = "";
-		try {
-			apiKey = settings.getApiKey();
-		} catch (PropertyAccessException e) {
-			e.printStackTrace();
-		}
-
-		//FOR DEBUGGING: APIKEY 4b45808a4d1a83471866761a8d7e5325
-		//We can continue with logging in if the API key has been set before already
-		if(apiKey != null && !apiKey.equals("null") && !apiKey.isEmpty()) {
-			messageHelper.showMessage("Api key is already stored, going to main screen! Key: " + apiKey);
-			goToMainScreen();
-		}
+		messageHelper = new MessageHelper(rootParent);
+        delayedTaskHelper = new DelayedTaskHelper();
 
 		//FOR DEBUGGING: REMOVE LATER
 		apiKeyTextField.setText("4b45808a4d1a83471866761a8d7e5325");
+
+        try {
+            userSettings = new UserSettings();
+        } catch (PropertyLoadException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            apiKey = userSettings.getApiKey();
+        } catch (EmptyValueException e) {
+            e.printStackTrace();
+        }
+
+        listenToWindowShown();
+
 	}
 
-	public void handleSubmitApiKey(ActionEvent actionEvent) {
+	@Override
+    public void listenToWindowShown() {
+        Task<Void> checkApiTask = new Task<Void>() {
+            @Override
+            protected Void call() throws Exception {
+                Platform.runLater( () -> checkForSavedApiKey());
+                return null;
+            }
+        };
+
+        delayedTaskHelper.delayedTask(checkApiTask, FXConstants.DEFAULT_DELAYED_TASK_TIME);
+    }
+
+
+    /**
+     * Design decision: this is run later because running goToMainScreen() in initialize() would not have required
+     * fields available, such as screenParent, because in ScreenController, we have to first initialize the FxmlLoader
+     * which in turn will run the initialize() method on all of our controllers. But at this point, our screenParent
+     * field has not been assigned yet in any controller, because the screen controller is gotten from the FxmlLoader.
+     *
+     * So to avoid NullPointerException where we try to reference a screenParent that doesn't exist yet, we have to
+     * delay the running of this method to when window is actually showing, which is done in listenToWindowShown()
+     *
+     */
+	private void checkForSavedApiKey() {
+        //FOR DEBUGGING: APIKEY 4b45808a4d1a83471866761a8d7e5325
+        //We can continue with logging in if the API key has been set before already
+        if(apiKey != null && !apiKey.isEmpty()) {
+            messageHelper.showMessage("API key is already stored, logging in...");
+            goToMainScreen();
+        }
+
+    }
+
+	public void handleSubmitApiKey() {
 		String apiKey = apiKeyTextField.getText();
 
-		apiService.getResponse(apiKey, new Callback<Response>() {
-			@Override public void success(final Response response, final Response response2) {
-				//UI can't be updated from non-application thread, run this later on the UI thread.
-				//Set API key in properties file, so the user only needs to enter it once.
-				settings.setApiKey(apiKeyTextField.getText());
-				Platform.runLater( () -> messageHelper.showMessage("Success! You're being logged in..."));
-				goToMainScreen();
-			}
+        apiAdapater = new ApiAdapater(apiKey);
+        apiService = apiAdapater.getApiService();
 
-			@Override public void failure(final RetrofitError retrofitError) {
-				// -11-
-				Platform.runLater( () -> messageHelper.showMessage("Your API key is incorrect or invalid"));
-			}
-		});
+		apiService.getResponse().enqueue(new Callback<Void>() {
+
+            @Override
+            public void onResponse(Call<Void> call, Response<Void> loginValidation) {
+                //UI can't be updated from non-application thread, run this later on the UI thread.
+                //Set API key in properties file, so the user only needs to enter it once.
+                if(loginValidation.isSuccessful()) {
+                    try {
+                        userSettings.setApiKey(apiKeyTextField.getText());
+                        Platform.runLater(() -> messageHelper.showMessage("Success! You're being logged in..."));
+                        goToMainScreen();
+                    } catch (IOException e) {
+                        messageHelper.showMessage("Could not save your API key.");
+                        e.printStackTrace();
+                    }
+                } else {
+                    Platform.runLater( () -> messageHelper.showMessage("Could not reach authentication services"));
+                }
+            }
+
+            @Override
+            public void onFailure(Call<Void> call, Throwable throwable) {
+                Platform.runLater( () -> messageHelper.showMessage("Your API key is incorrect or invalid"));
+            }
+        });
+
 	}
 
-	public void goToMainScreen() {
-		screenController.setScreen(CineMateApplication.MAIN_SCREEN_ID);
+	private void goToMainScreen() {
+		screenParent.setScreen(CineMateApplication.MAIN_SCREEN_ID);
 	}
 
-	@Override public void setScreenParent(ScreenController screenParent) {
-		this.screenController = screenParent;
+
+    @Override public void setScreenParent(ScreenController screenParent) {
+		this.screenParent = screenParent;
 	}
 }
