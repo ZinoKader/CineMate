2a1c67fea2b57c6efd998560953814f4ccccb77c
zinokader <zino@zinokader.se>
2017-04-09 23:36:59 +0200

    Clarified error message.

--- src/main/exceptions/PropertyAccessException.java
+++ src/main/exceptions/PropertyAccessException.java
@@ -7,7 +7,7 @@ package main.exceptions;
 public class PropertyAccessException extends Exception {
 
     public PropertyAccessException() {
-        super("Could not access user data. The properties were not loaded correctly.");
+        super("Could not access user data from the properties file. Was the data stored correctly?");
     }
 
 }
