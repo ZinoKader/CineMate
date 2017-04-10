fd6aad75b8dd442abd40dceb09c2d89da30da4bb
zinokader <zino@zinokader.se>
2017-04-10 17:51:19 +0200

    Shorter default delayed task time

--- src/main/constants/FXConstants.java
+++ src/main/constants/FXConstants.java
@@ -4,7 +4,7 @@ public final class FXConstants {
 
     public static final int DOUBLE_CLICK_COUNT = 2;
     public static final double DEFAULT_DELAYED_CLOSE_TIME = 1.5;
-    public static final double DEFAULT_DELAYED_TASK_TIME = 2;
+    public static final double DEFAULT_DELAYED_TASK_TIME = 1.5;
 
     private FXConstants() {}
 
